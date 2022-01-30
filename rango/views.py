b80dc23b488070aa8e19bf755a5b7459c44cd7a6
Cheng Liu <983123963@qq.com>
2022-01-30 18:10:09 +0000

    chatper4

--- rango/views.py
+++ rango/views.py
@@ -4,8 +4,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<a href='/rango/about/'>About</a>Rango says hey there partner!")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
-    return HttpResponse("<a href='/rango/'>Index</a>Rango says here is the about page.")
+    context_dict = {'name': 'Cheng Liu'}
+    return render(request, 'rango/about.html', context=context_dict)
